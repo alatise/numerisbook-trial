2cc3298fb354fcf233d180cb2b17782b7bec2a0d
Alatise <fayobio17@gmail.com>
2024-09-04 01:03:40 +0100

    deployment fix

--- src/components/SidebarComp.jsx
+++ src/components/SidebarComp.jsx
@@ -1,6 +1,12 @@
 import { Layout, Menu } from "antd";
 import { useNavigate, useLocation } from "react-router-dom";
 import logo from "../assets/sidebar/logo.svg";
+import startedIcon from "../assets/sidebar/started.svg";
+import overviewIcon from "../assets/sidebar/overview.svg";
+import invoiceIcon from "../assets/sidebar/invoice.svg";
+import beneficiaryIcon from "../assets/sidebar/beneficiary.svg";
+import helpIcon from "../assets/sidebar/help.svg";
+import settingIcon from "../assets/sidebar/setting.svg";
 
 const { Sider } = Layout;
 
@@ -11,37 +17,37 @@ const Sidebar = () => {
   const menuItems = [
     {
       key: "/getting-started",
-      icon: <img src="/src/assets/sidebar/started.svg" />,
+      icon: <img src={startedIcon} alt="Getting Started" />,
       label: "Getting Started",
     },
     {
       key: "/overview",
-      icon: <img src="src/assets/sidebar/overview.svg" />,
+      icon: <img src={overviewIcon} alt="Overview" />,
       label: "Overview",
     },
     {
       key: "/accounts",
-      icon: <img src="src/assets/sidebar/started.svg" />,
+      icon: <img src={startedIcon} alt="Accounts" />,
       label: "Accounts",
     },
     {
       key: "/invoice",
-      icon: <img src="src/assets/sidebar/invoice.svg" />,
+      icon: <img src={invoiceIcon} alt="Invoice" />,
       label: "Invoice",
     },
     {
       key: "/beneficiary",
-      icon: <img src="src/assets/sidebar/beneficiary.svg" />,
+      icon: <img src={beneficiaryIcon} alt="Beneficiary Management" />,
       label: "Beneficiary Management",
     },
     {
       key: "/help-center",
-      icon: <img src="src/assets/sidebar/help.svg" />,
+      icon: <img src={helpIcon} alt="Help Center" />,
       label: "Help Center",
     },
     {
       key: "/settings",
-      icon: <img src="src/assets/sidebar/setting.svg" />,
+      icon: <img src={settingIcon} alt="Settings" />,
       label: "Settings",
     },
   ];
